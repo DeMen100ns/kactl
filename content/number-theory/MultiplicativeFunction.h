eb8ed863a224b3c28852c4dec4c2b8211c36ec8d
Vo Khac Trieu <72329092+DeMen100ns@users.noreply.github.com>
2024-10-01 15:06:48 +0700

    Create MultiplicativeFunction.h

---
+++ content/number-theory/MultiplicativeFunction.h
@@ -0,0 +1,42 @@
+vector <int> sieve (int n) {
+    vector <int> prime, is_composite, func, cnt;
+    vector <vector<int>> primepow;
+
+    auto powpk = [&](int p, int k) -> int{
+        return primepow[p][k];
+    };
+
+    auto f = [&](int p, int k) -> int{
+        //calculate f(p^k)
+    };
+
+    is_composite.resize(n, 0);
+    func.resize(n, 0);
+    cnt.resize(n, 0);
+    primepow.resize(n);
+
+	func[1] = 1;
+	for (int i = 2; i < n; ++i) {
+		if (!is_composite[i]) {
+			prime.push_back (i);
+            for(int a = 1, pw = 0; a < n; a *= i, pw++){
+                primepow[i].push_back(a);
+            }
+			func[i] = f(i, 1); cnt[i] = 1;
+		}
+		for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
+			is_composite[i * prime[j]] = true;
+			if (i % prime[j] == 0) {
+                //f(i * prime[j]) = f(i / prime[j] ^ cnt[i]) * f(prime[j] ^ (cnt[i] + 1))
+				func[i * prime[j]] = func[i / powpk(prime[j], cnt[i])] * f(prime[j], cnt[i] + 1);
+				cnt[i * prime[j]] = cnt[i] + 1;
+				break;
+			} else {
+                //f(i * prime[j]) = f(i) * f(prime[j])
+				func[i * prime[j]] = func[i] * func[prime[j]];
+				cnt[i * prime[j]] = 1;
+			}
+		}
+	}
+    return func;
+}
