aaad23ffcc0e20676c68074eb777490bb2f48e1f
Vo Khac Trieu <72329092+DeMen100ns@users.noreply.github.com>
2024-10-04 21:28:39 +0700

    Update CustomBitset.h

--- content/data-structures/CustomBitset.h
+++ content/data-structures/CustomBitset.h
@@ -1,3 +1,10 @@
+/**
+ * Author: DeMen100ns
+ * Date: 2024-01-01
+ * Description: Custom bitset
+ * Usage: bitset<len> in your function.
+ */
+
 template <int len = 1>
 int calc(int n) {
     if (n > len) {
